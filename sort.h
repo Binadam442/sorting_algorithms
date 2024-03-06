1054a73f6a70e33e2fb25aaf3a1f102d62c92f59
Binadam442 <sanusiadam0000@gmail.com>
2024-03-06 04:04:11 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -1,14 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stddef.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * enum bool_e - manual definition of booleans, in case C99 stdbool.h not
- * available
- * @false: logical 0
- * @true: logical 1
+ * enum bool - struct for bool type
+ *@false: false is equivalent to 0
+ *@true: equivalent to 1
+ * Return: nothing
  */
-typedef enum bool_e
+typedef enum bool
 {
 	false = 0,
 	true
@@ -28,44 +30,37 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* supplied functions */
+
+/* aux functions */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_bubble(int *a, int *b);
+int partition(int *array, size_t size, int low, int high);
+void partition_sort(int *array, size_t size, int low, int high);
+void merge_sort_call(int *subarray, int *buffer, size_t lo, size_t hi);
+void merge_sub(int *subarray, int *buffer, size_t lo, size_t mid, size_t hi);
+void partition_sort_hoare(int *array, size_t size, int low, int high);
+int partition_hoare(int *array, size_t size, int low, int high);
+void swap_tail(listint_t **list, listint_t **head, listint_t **tail);
+void swap_head(listint_t **list, listint_t **tail, listint_t **head);
+int get_max(int *array, int size);
+void lsd_sort(int *array, size_t size, int exp, int *copy);
 
-/* mandatory tasks */
+
+
+/* algorithm functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* quick_sort() helpers */
-int partition(int *array, int low, int high, size_t size);
-void quicksort(int *array, int low, int high, size_t size);
-
-/* advanced tasks */
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-/* void sort_deck(deck_node_t **deck); */
+void cocktail_sort_list(listint_t **list);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
-/* cocktail_sort_list() helper */
-void dll_adj_swap(listint_t **list, listint_t *left, listint_t *right);
 
-/* merge_sort() helpers */
-void PrintArray(int *array, int iBeg, int iEnd);
-void CopyArray(int *array, int iBeg, int iEnd, int *work_copy);
-void TopDownMerge(int *array, int iBeg, int iMid, int iEnd, int *work_copy);
-void TopDownSplitMerge(int *work_copy, int iBeg, int iEnd, int *array);
-
-/* heap_sort() helpers */
-int iParent(int i);
-int iLeftChild(int i);
-void ConvertToHeap(int *array, size_t size);
-void SiftDown(int *array, size_t size, int start, int end);
-
-#endif /* SORT_H */
+#endif
